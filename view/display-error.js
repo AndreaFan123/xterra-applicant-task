1b0aa51149c0939dfd7cefc391bee8d33e6faf0e
Andrea Fan <andrea.fan@ringus-solution.com>
2024-08-27 17:59:13 +0800

    Update: Move README in the root folder

--- view/display-error.js
+++
@@ -1,7 +0,0 @@
-export function displayErrorMessage() {
-  const errorMessageContainer = document.querySelector("#error-msg");
-  const errorMessage = document.createElement("span");
-  errorMessage.textContent = "Something went wrong! We will fix immediately";
-  errorMessageContainer.appendChild(errorMessage);
-  table.style.display = "none";
-}
